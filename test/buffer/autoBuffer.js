5071383ebb50053845f8bc1a5ada714353d23eb8
yo2dh <yo2dh@naver.com>
2013-12-27 15:43:08 +0900

    AutoBuffer was added.

--- test/buffer/autoBuffer.js
+++ test/buffer/autoBuffer.js
@@ -3,7 +3,7 @@ var AutoBuffer = require( '../../lib/buffer/autoBuffer' );
 var testBase = process.cwd() + '/test';
 
 describe( 'AutoBuffer' , function() {
-    describe( '#AutoBuffer' , function() {
+    describe( '#Append&Pop' , function() {
         it( 'should append and pop correctly' , function( done ) {
 
             var autoBuffer = new AutoBuffer ( 5 );
@@ -36,12 +36,51 @@ describe( 'AutoBuffer' , function() {
             }
             length = autoBuffer.getLength()
             buffer = autoBuffer.getBuffer();
-            for ( var i = 0 ; i < length ; i++ )
             {
-                buffer[i].should.equal( buf[(i+12)%8] );
+                for ( var i = 0 ; i < length ; i++ )
+                {
+                    buffer[i].should.equal( buf[(i+12)%8] );
+                }
             }
             done();
-         })
+         }),
+        it( 'should pop and append correctly' , function( done ) {
+
+            var autoBuffer = new AutoBuffer ( 5 );
+            var buf = new Buffer ( 10 );
+            {
+                for ( var i = 0 ; i < buf.length ; i++ )
+                {
+                    buf[i] = i;
+                }
+                for ( var j = 0 ; j < 10 ; j++ )
+                {
+                    autoBuffer.append( buf );
+                }
+            }
+            var popBuffer = new Buffer( 5 );
+            {
+                var even = 0;
+                for ( var i = 0 ; i < 20 ; i++ )
+                {
+                    if ( i % 2 == 0 )
+                    {
+                        even = 0;
+                    }
+                    else
+                    {
+                        even = 5;
+                    }
+                    autoBuffer.pop( popBuffer )
+                    for ( var j = 0 ; j < popBuffer.length ; j++ )
+                    {
+                        popBuffer[j].should.equal( even + j )
+                    }
+
+                }
+            }
+            done();
+        })
     });
 
 });
