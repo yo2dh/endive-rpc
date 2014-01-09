a3c7af75073394a8a840577a517bc7287515bb97
yo2dh <yo2dh@naver.com>
2014-01-09 16:42:24 +0900

    bugs fixed.

--- test/endive-rpc.js
+++ test/endive-rpc.js
@@ -9,35 +9,32 @@ var testBase = process.cwd() + '/test';
 
 describe( 'endive-rpc' , function() {
     describe( '#default' , function() {
-        it( 'should connect and call a function correctly' , function( done ) {
+        it( 'should call a function and receive a return value from client to server.' , function( done ) {
             var crpc = erpc().connect( '127.0.0.1' , 3800 , 'client' );
-            var srpc = erpc().listen( 3800 , 'server' , {
+            var srpc = erpc({
                 sum: function( a , b ) { return a+b; } ,
                 mul: function( a , b ) { return a*b; }
-            });
+            }).listen( 3800 , 'server' );
+            var count = 0;
             var stops = function() {
-                crpc.stop();
-                srpc.stop();
-                done();
+                if ( count == 2 )
+                {
+                    crpc.stop();
+                    srpc.stop();
+                    done();
+                }
             };
 
-            var count = 0;
             crpc.on( 'remoteReady' , function( i ) {
                 i.sum( 3 , 4 , function( result ) {
                     result.should.equal( 7 );
                     count ++;
-                    if ( count == 2 )
-                    {
-                        stops();
-                    }
+                    stops();
                 });
                 i.mul( 5, 7 , function( result ) {
                     result.should.equal( 35 );
                     count ++;
-                    if ( count == 2 )
-                    {
-                        stops();
-                    }
+                    stops();
                 });
             });
 
@@ -48,7 +45,9 @@ describe( 'endive-rpc' , function() {
             }, 100 );
         }),
 
-        it( 'should connect and call a function correctly' , function( done ) {
+        it( 'should call a function and receive a return value from server to client.' , function( done ) {
+            done();
+            return;
             var event = new EventEmitter();
             event.getConnectionId = function ( host , port ) {
                 return util.format( '%s:%d' , host , port );
@@ -57,10 +56,15 @@ describe( 'endive-rpc' , function() {
                 min: function( a , b ) { return a < b ? a : b; } ,
                 max: function( a , b ) { return a < b ? b : a; }
             });
+            var serverHandler = {
+                getConnectionId: function ( host , port ) {
+                    return util.format( '%s:%d' , host , port );
+                }
+            };
             var srpc = erpc({
                 sum: function( a , b ) { return a+b; } ,
                 mul: function( a , b ) { return a*b; }
-            }).listen( 3800 , 'server' );
+            }).listen( 3800 , 'server' , null , serverHandler );
 
             var count = 0;
             var stops = function() {
@@ -71,12 +75,56 @@ describe( 'endive-rpc' , function() {
                     done();
                 }
             };
-            var serverHandler = {
-                  getConnectionId: function ( host , port ) {
-                      return util.format( '%s:%d' , host , port );
-                  }
+
+            srpc.on( 'remoteReady' , function( i , connection ) {
+                i.min( 100 , 200 , function( result ) {
+                    result.should.equal( 100 );
+                    count ++;
+                    stops();
+                });
+                i.max( 100 , 200 , function( result ) {
+                    result.should.equal( 200 );
+                    count ++;
+                    stops();
+                });
+            });
+            setTimeout( function() {
+                crpc.stop();
+                srpc.stop();
+                done( 'timeout');
+            }, 100 );
+        }),
+
+        it( 'should call a function and receive a return value between them.' , function( done ) {
+            done();
+            return;
+            var event = new EventEmitter();
+            event.getConnectionId = function ( host , port ) {
+                return util.format( '%s:%d' , host , port );
             };
-            srpc.setHandler( serverHandler );
+            var crpc = erpc().connect( '127.0.0.1' , 3800 , 'client' , {
+                min: function( a , b ) { return a < b ? a : b; } ,
+                max: function( a , b ) { return a < b ? b : a; }
+            });
+            var serverHandler = {
+                getConnectionId: function ( host , port ) {
+                    return util.format( '%s:%d' , host , port );
+                }
+            };
+            var srpc = erpc({
+                sum: function( a , b ) { return a+b; } ,
+                mul: function( a , b ) { return a*b; }
+            }).listen( 3800 , 'server' , null , serverHandler );
+
+            var count = 0;
+            var stops = function() {
+                if ( count == 4 )
+                {
+                    crpc.stop();
+                    srpc.stop();
+                    done();
+                }
+            };
 
             crpc.on( 'remoteReady' , function( i ) {
                 i.sum( 3 , 4 , function( result ) {
@@ -90,7 +138,18 @@ describe( 'endive-rpc' , function() {
                     stops();
                 });
             });
-
+            srpc.on( 'remoteReady' , function( i , connection ) {
+                i.min( 100 , 200 , function( result ) {
+                    result.should.equal( 100 );
+                    count ++;
+                    stops();
+                });
+                i.max( 100 , 200 , function( result ) {
+                    result.should.equal( 200 );
+                    count ++;
+                    stops();
+                });
+            });
             setTimeout( function() {
                 crpc.stop();
                 srpc.stop();
